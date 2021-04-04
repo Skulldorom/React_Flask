7e860e5ad25b0ec380e99cd448a6eec2dbc5fc8e
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2021-04-04 16:48:23 +0300

    Update index.js
    Auto set debug

--- front/src/index.js
+++ front/src/index.js
@@ -4,10 +4,12 @@ import Base from './Base';
 import '../node_modules/bulma/css/bulma.min.css';
 import './Base.css';
 
-const debug = true;
-
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
+    var debug = true;
+else
+    var debug = false;
+  
 const backend = debug ?'http://localhost:5000' : '';
-
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Base  debug/>, document.getElementById('root'));
